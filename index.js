5c9de0654ffef32d04f106a37094609d9cecde6e
johaolopz <johao.lopz.m@gmail.com>
2021-12-18 00:59:26 -0500

    mongoDB connection

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+var mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost:27017/api_nodejs_db')
+    .then(()=>{
+        console.log('Connection is Ok')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+    
