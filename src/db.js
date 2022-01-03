4a902cb4e762e77a27c77e4447689448750cd23d
johaolopz <johao.lopz.m@gmail.com>
2022-01-03 11:09:29 -0500

    deployment Vs3

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose")
 
 //'mongodb://localhost:27017/api_nodejs_db'
-mongoose.connect('mongodb+srv://api_nodejs:Api_nodejs@cluster0.zqcvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://api_nodejs:Api_nodejs@cluster0.zqcvn.mongodb.net/test')
     .then(()=>{
         console.log('Connection is Ok')
     })
