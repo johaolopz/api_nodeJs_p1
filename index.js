1911ca981bd166b319021930244d606c6ed26a87
johaolopz <johao.lopz.m@gmail.com>
2022-01-03 11:48:46 -0500

    dotenv installed and config

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict'
 const app = require('./src/app')
+require('dotenv').config()
 
 // //connection is called
 // const conn = require('./src/db')
 
-//PORT defined
-const port = 3001
+// //PORT defined
+// const port = 3001
 
 //Server is uploaded
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server listening at 3001')
   });
 
