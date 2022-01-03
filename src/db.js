5ad86ae9029bc78505a6f8b8ff91dc020d07da2b
johaolopz <johao.lopz.m@gmail.com>
2022-01-03 10:30:39 -0500

    cloud.mongodb uri config

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 'use strict'
 const mongoose = require("mongoose")
 
-mongoose.connect('mongodb://localhost:27017/api_nodejs_db')
+mongoose.connect('mongodb+srv://api_nodejs:Api_nodejs@cluster0.zqcvn.mongodb.net/cluster0?retryWrites=true&w=majority' || 'mongodb://localhost:27017/api_nodejs_db')
     .then(()=>{
         console.log('Connection is Ok')
     })
