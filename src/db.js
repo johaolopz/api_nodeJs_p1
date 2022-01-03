e19fa61e83221ae85605d1bc790a622f636978d4
johaolopz <johao.lopz.m@gmail.com>
2022-01-03 12:42:42 -0500

    Database changed

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 require('dotenv').config()
 
 //'mongodb://localhost:27017/api_nodejs_db'
-mongoose.connect(`mongodb+srv://api_nodejs:${process.env.PASSWORD}@cluster0.zqcvn.mongodb.net/test`)
+mongoose.connect(`mongodb+srv://api_nodejs:${process.env.PASSWORD}@cluster0.zqcvn.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`)
     .then(()=>{
         console.log('Connection is Ok')
     })
